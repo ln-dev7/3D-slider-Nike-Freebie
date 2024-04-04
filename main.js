0c21ee9ff35714c275e344dec71ac244026ec03d
LN <leonelngoya@gmail.com>
2024-04-04 08:10:03 +0100

    first commit

--- main.js
+++ main.js
@@ -1,2 +1,33 @@
-import './style.scss'
-import gsap from 'gsap'
+import "./style.scss";
+import gsap from "gsap";
+
+const items = document.querySelectorAll(".item");
+const shoes = document.querySelector(".shoes");
+const texts = document.querySelectorAll(".text");
+
+items.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    gsap.to(shoes, {
+      duration: 2,
+      y: `-${index * 100}%`,
+      ease: "elastic.out(1,0.75)",
+    });
+    texts.forEach((text, i) => {
+      if (i === index) {
+        gsap.to(text, {
+          duration: 1,
+          opacity: 0,
+          y: 50,
+          ease: "elastic.out(1,0.75)",
+        });
+      } else {
+        gsap.to(text, {
+          duration: 1,
+          opacity: 1,
+          y: -50,
+          ease: "elastic.out(1,0.75)",
+        });
+      }
+    });
+  });
+});
